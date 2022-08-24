c6af67428e4f0c347046be3479236bb1e385ddec
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2022-08-24 20:54:22 -0300

    Criando models de mensagem e topico

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room
+from .models import Room, Messages, Topic
 
 admin.site.register(Room)
+admin.site.register(Messages)
+admin.site.register(Topic)
