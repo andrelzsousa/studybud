686878b691cf16f1e10609e1c8f7f0a83600775c
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2022-08-31 22:24:00 -0300

    aba de tópicos

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 '''rooms = [
@@ -10,8 +10,16 @@ from .forms import RoomForm
 ]'''
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    if request.GET.get('q') != None:
+        q = request.GET.get('q')
+    else:
+        q = ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
